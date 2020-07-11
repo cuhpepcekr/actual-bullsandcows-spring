b7427e9ea7044f1a2ee35dbc12a656cf545b4d70
cuhpepcekr <lintogm@gmail.com>
2020-07-11 19:45:42 +0900

    Swagger 추가.

--- src/main/java/com/cuhpepcekr/bullsandcows/controller/UserController.java
+++ src/main/java/com/cuhpepcekr/bullsandcows/controller/UserController.java
@@ -2,6 +2,8 @@ package com.cuhpepcekr.bullsandcows.controller;
 
 import com.cuhpepcekr.bullsandcows.entity.User;
 import com.cuhpepcekr.bullsandcows.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = {"User"})
 @RestController()
 @RequestMapping(value = "/api")
 public class UserController {
@@ -16,6 +19,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @ApiOperation(value = "모든 회원 조회", notes = "모든 회원을 조회합니다.")
     @GetMapping(value = "/users")
     public List<User> getUsers() {
         return userService.getUsers();
