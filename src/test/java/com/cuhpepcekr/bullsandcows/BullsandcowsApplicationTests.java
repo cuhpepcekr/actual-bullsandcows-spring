2430a6c70fe5d2e0eb1468222575076319683873
clover <lintogm@gmail.com>
2020-07-14 09:53:09 +0900

    초기화. (README.md, .gitignore 제외.)

--- src/test/java/com/cuhpepcekr/bullsandcows/BullsandcowsApplicationTests.java
+++ src/test/java/com/cuhpepcekr/bullsandcows/BullsandcowsApplicationTests.java
@@ -1,32 +1,13 @@
 package com.cuhpepcekr.bullsandcows;
 
-import com.cuhpepcekr.bullsandcows.entity.User;
-import com.cuhpepcekr.bullsandcows.service.SignService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class BullsandcowsApplicationTests {
-
-    @Autowired
-    SignService signService;
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    void initialData() {
-        List<User> users = new ArrayList<User>();
-        users.add(User.builder().username("test1").password("1234").name("testname1").build());
-        users.add(User.builder().username("test2").password("1234").name("testname2").build());
-
-        for (User user : users) {
-            signService.signUp(user);
-        }
-    }
 }
